4be7b10cb465cb62323276ccf82f3a3421472c8b
Shoptorshi Jamil <sjami002@gmail.com>
2019-04-23 22:34:51 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,4 +2,20 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do
+    erb :index  
+  end
+  
+  get '/new' do
+    erb :create_puppy 
+  end
+  
+  post '/' do
+    
+    new_puppy = Puppy.new(params[:name], params[:breed], params[:months_old])
+    
+    erb :display_puppy 
+  end
+  
+
 end
